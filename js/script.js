c6ce033011f4e8efbf88dbb063fc67cd84756f50
sHa <sha@shadoll.dev>
2022-12-01 21:32:15 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/sw.js', { scope: './' })
+		navigator.serviceWorker.register('sw.js', { scope: './' })
 		.then(function(registration) {
 			console.log('ServiceWorker registration successful with scope: ', registration.scope);
 		})
